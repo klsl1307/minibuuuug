248bd1d478e5c1bd86f90357c3d3eb8a3bbe9e89
klsl1307 <34433617+klsl1307@users.noreply.github.com>
2020-10-15 11:51:25 +0800

    v2

--- Assets/StartGame.cs
+++ Assets/StartGame.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Choose : MonoBehaviour
+public class StartGame : MonoBehaviour
 {
 
     // Use this for initialization
